ffa64016d584c59255f59ea3bbf1d592090071ea
hojun.yoon <tjdsksro90@gmail.com>
2024-06-14 12:02:13 +0900

    ⚡️ optimize: image 관련 config 수정

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       "www.notion.so",
       "images.unsplash.com",
       "s3.us-west-2.amazonaws.com",
+      "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
   },
 };

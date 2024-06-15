fca19fc1db2385fed6cade201b29c08374c1b9a0
hojun.yoon <tjdsksro90@gmail.com>
2024-06-15 10:49:28 +0900

    💄 style: css 추가

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -21,20 +21,20 @@ const ProjectItem = ({ data }: Props) => {
 
   // 색상 매핑 객체
   const colorMap: { [key: string]: string } = {
-    red: "bg-custom-red",
-    blue: "bg-custom-blue",
-    purple: "bg-custom-purple",
-    gray: "bg-custom-gray",
-    brown: "bg-custom-brown",
-    green: "bg-custom-green",
-    orange: "bg-custom-orange",
-    pink: "bg-custom-pink",
-    yellow: "bg-custom-yellow",
+    red: "bg-custom-red dark:bg-custom-red-dark",
+    orange: "bg-custom-orange dark:bg-custom-orange-dark",
+    yellow: "bg-custom-yellow dark:bg-custom-yellow-dark",
+    green: "bg-custom-green dark:bg-custom-green-dark",
+    blue: "bg-custom-blue dark:bg-custom-blue-dark",
+    purple: "bg-custom-purple dark:bg-custom-purple-dark",
+    pink: "bg-custom-pink dark:bg-custom-pink-dark",
+    brown: "bg-custom-brown dark:bg-custom-brown-dark",
+    gray: "bg-custom-gray dark:bg-custom-gray-dark",
   };
 
   // 클래스 네임 설정 함수
   const getColorClassName = (color: string) => {
-    return colorMap[color] || "bg-custom-default"; // 기본값 설정 가능
+    return colorMap[color] || "bg-custom-default dark:bg-custom-default-dark"; // 기본값 설정 가능
   };
 
   const calculatedPeriod = (start: string, end: string): number => {

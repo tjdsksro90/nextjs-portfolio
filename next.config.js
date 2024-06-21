521ce94671499e2dc097eb571cfe73f1cc3f46e3
hojun.yoon <tjdsksro90@gmail.com>
2024-06-21 13:20:49 +0900

    🔨 [setting] next config 수정

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "www.notion.so",
-      "images.unsplash.com",
-      "s3.us-west-2.amazonaws.com",
-      "prod-files-secure.s3.us-west-2.amazonaws.com",
+      'www.notion.so',
+      'images.unsplash.com',
+      's3.us-west-2.amazonaws.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com',
     ],
+    format: ['image/png', 'image/webp', 'image/jpeg'],
   },
 };
 

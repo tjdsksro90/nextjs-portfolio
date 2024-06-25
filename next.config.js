6aee9f6e624e69a96c6cbf85603ac4c190ed4460
hojun.yoon <tjdsksro90@gmail.com>
2024-06-26 08:37:38 +0900

    🩹 fix: 주석 제거

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
       's3.us-west-2.amazonaws.com',
       'prod-files-secure.s3.us-west-2.amazonaws.com',
     ],
-    // format: ['image/png', 'image/webp', 'image/jpeg'],
     path: '/_next/image',
     loader: 'default',
   },

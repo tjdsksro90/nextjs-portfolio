3c60725ef3998315afb5b483dda9eda114198388
hojun.yoon <tjdsksro90@gmail.com>
2024-06-27 00:01:42 +0900

    ♻️ refactor: 기간 코드 리팩토링
    - end 없을 시 진행 중으로 표시
    - 0주일 시 요일로 표시되게끔 수정

--- components/common/period.tsx
+++ components/common/period.tsx
@@ -1,15 +1,21 @@
-import { calculatedPeriod } from "@/utils/period-mapping";
-import React from "react";
+import { calculatedPeriod } from '@/utils/period-mapping';
+import React from 'react';
 
 interface Props {
   start: string;
-  end: string;
+  end: string | null;
 }
 
 const CommonPeriod = ({ start, end }: Props) => {
   return (
     <p className="my-1 ">
-      작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}주)
+      {end ? (
+        <>
+          작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)})
+        </>
+      ) : (
+        <>작업기간 : {start} ~ (진행 중)</>
+      )}
     </p>
   );
 };
--- utils/period-mapping.ts
+++ utils/period-mapping.ts
@@ -1,21 +1,21 @@
-export const calculatedPeriod = (start: string, end: string): number => {
-  const startDateStringArray = start.split("-");
-  const endDateStringArray = end.split("-");
+export const calculatedPeriod = (start: string, end: string): string => {
+  const startDateStringArray = start.split('-');
+  const endDateStringArray = end.split('-');
 
   var startDate = new Date(
     Number(startDateStringArray[0]),
     Number(startDateStringArray[1]) - 1, // 월은 0부터 시작하므로 -1 필요
-    Number(startDateStringArray[2])
+    Number(startDateStringArray[2]),
   );
   var endDate = new Date(
     Number(endDateStringArray[0]),
     Number(endDateStringArray[1]) - 1, // 월은 0부터 시작하므로 -1 필요
-    Number(endDateStringArray[2])
+    Number(endDateStringArray[2]),
   );
 
   const diffInMs = Math.abs(endDate.getTime() - startDate.getTime());
   const days = diffInMs / (1000 * 60 * 60 * 24);
   const result = Math.floor(days / 7);
 
-  return result;
+  return result === 0 ? `${days}일` : `${result}주`;
 };

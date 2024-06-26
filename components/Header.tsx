59d7d1d4028cd948d834d03697b7712045393ab5
hojun.yoon <tjdsksro90@gmail.com>
2024-06-27 00:03:42 +0900

    🎨 polish: header link 연결 및 구조 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import ThemeToggle from "./ThemeToggle";
+import Link from 'next/link';
+import ThemeToggle from './ThemeToggle';
 
 function Header() {
   return (
     <header className="text-gray-600 body-font">
-      <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+      <div className="flex flex-col flex-wrap items-center p-5 md:container md:mx-auto md:flex-row">
         <Link href="/" legacyBehavior>
           <a className="flex items-center mb-4 font-medium title-font md:mb-0">
             <svg
@@ -23,14 +23,16 @@ function Header() {
           </a>
         </Link>
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-          <Link className="mr-5" href={"/"}>
+          <Link className="mr-5" href={'/'}>
             홈
           </Link>
-          <Link className="mr-5" href={"/projects"}>
+          <Link className="mr-5" href={'/projects'}>
             프로젝트
           </Link>
-          <Link className="mr-5" href={"/"}>
-            연락하기
+          <Link legacyBehavior href={'https://open.kakao.com/o/sKGqxszg'}>
+            <a className="mr-5" target="_blank" rel="noopener noreferrer">
+              연락하기
+            </a>
           </Link>
         </nav>
         <ThemeToggle />

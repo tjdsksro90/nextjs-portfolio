ae8c984012e92c7499a5610e1f88df86dcf800ed
hojun.yoon <tjdsksro90@gmail.com>
2024-06-26 17:44:55 +0900

    🎨 polish: 메인 animaion 변경 #1

--- components/home/Animation.tsx
+++ components/home/Animation.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import Lottie from "react-lottie-player";
+import Lottie from 'react-lottie-player';
 // Alternatively:
 // import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
 
-import lottieJson from "../../public/animaion.json";
+import lottieJson from '../../public/animaion.json';
 
 export default function Animation() {
-  return <Lottie loop animationData={lottieJson} play />;
+  return <Lottie loop={false} animationData={lottieJson} play />;
 }

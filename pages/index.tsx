6345968e1d12cd4f7b57ca8753685ffa364c0838
hojun.yoon <tjdsksro90@gmail.com>
2024-06-27 00:05:21 +0900

    💄 style: 메인화면 클래스 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Seo from "@/components/Seo";
-import Hero from "@/components/home/Hero";
+import Seo from '@/components/Seo';
+import Hero from '@/components/home/Hero';
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <Seo title="Home" />
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <section className="flex flex-col items-center justify-center min-h-screen text-gray-600 body-font">
+        <div className="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">
           <Hero />
         </div>
       </section>

19657d99779a2b066887732fa238c739762f302f
hojun.yoon <tjdsksro90@gmail.com>
2024-07-05 13:03:09 +0900

    🎨 polish: 메인 페이지 좌우 여백 수정 #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/home/Hero';
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container px-5 mx-auto md:px-14">
       <Seo title="Home" />
       <section className="flex flex-col items-center justify-center min-h-screen text-gray-600 body-font">
         <div className="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row">

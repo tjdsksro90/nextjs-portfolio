983f5813c9f977fc644694735a4c59b32de339a7
hojun.yoon <tjdsksro90@gmail.com>
2024-07-16 18:34:16 +0900

    🩹 fix: 콘솔 에러나는 부분 수정 #3

--- pages/projects/[pageId].tsx
+++ pages/projects/[pageId].tsx
@@ -23,9 +23,9 @@ const ProjectDetail = ({ blocksData, pagesData }: ProjectDetailProps) => {
                 <svg
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>

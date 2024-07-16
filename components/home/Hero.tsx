33c613e413545dd728f0660eb0fef6e822bdaca5
hojun.yoon <tjdsksro90@gmail.com>
2024-07-16 18:33:08 +0900

    🩹 fix: 콘솔 에러나는 부분 수정 #1

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -20,9 +20,9 @@ export default function Hero() {
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 viewBox="0 0 24 24"
               >
                 <path d="M5 12h14M12 5l7 7-7 7"></path>

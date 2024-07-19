c75e9801e1a5b14052722c0568f0f41b712577cf
hojun.yoon <tjdsksro90@gmail.com>
2024-07-20 02:01:48 +0900

    🩹 fix: 코드 수정

--- api/lists.ts
+++ api/lists.ts
@@ -1,14 +1,14 @@
-import axios from "axios";
-import { TOKEN, DATABASE_ID } from "@/config/index";
+import axios from 'axios';
+import { TOKEN, DATABASE_ID } from '@/config/index';
 export const getLists = async () => {
   const options = {
-    method: "POST",
+    method: 'POST',
     url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
     // url: `https://api.notion.com/v1/databases/d2cd2c59eeea434aad1fb668a2d0ae98/query`,
     headers: {
-      accept: "application/json",
-      "Notion-Version": "2022-06-28",
-      "content-type": "application/json",
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
       Authorization: `Bearer ${TOKEN}`,
       // Authorization: `Bearer secret_OXDKakcJb8e7XBZGZpNIMkaHiXI6teTLIaaZu5Wn0WM`,
     },
@@ -17,10 +17,10 @@ export const getLists = async () => {
 
   axios
     .request(options)
-    .then(function (response) {
+    .then(response => {
       return response.data;
     })
-    .catch(function (error) {
+    .catch(error => {
       console.error(error);
     });
 };

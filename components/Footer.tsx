142fe002ac46f30d8e481a1ceab751e2024b9010
hojun.yoon <tjdsksro90@gmail.com>
2024-07-21 17:22:32 +0900

    ✨ update: link 수정 #9

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import MailIcon from '/public/icons/mail.svg';
 import GithubIcon from '/public/icons/github.svg';
 import TistoryIcon from '/public/icons/tistory.svg';
 import InstagramIcon from '/public/icons/instagram.svg';
+import CommonLink from './common/link';
 
 const Footer = () => {
   return (
@@ -21,35 +22,26 @@ const Footer = () => {
             </a>
           </p>
           <span className="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
-            <a
-              className="flex items-center justify-center text-gray-500 cursor-pointer"
+            <CommonLink
               href="mailto:tjdsksro90@gmail.com"
-              target="_blank"
-            >
-              <MailIcon className="w-5 h-5" fill="currentColor" />
-            </a>
-            <a
-              className="flex items-center justify-center ml-3 text-gray-500 cursor-pointer"
+              text={<MailIcon className="w-5 h-5" fill="currentColor" />}
+              className="flex items-center justify-center text-gray-500 cursor-pointer"
+            />
+            <CommonLink
               href="https://github.com/tjdsksro90"
-              target="_blank"
-            >
-              <GithubIcon className="w-5 h-5" fill="currentColor" />
-            </a>
-            <a
+              text={<GithubIcon className="w-5 h-5" fill="currentColor" />}
               className="flex items-center justify-center ml-3 text-gray-500 cursor-pointer"
+            />
+            <CommonLink
               href="https://gmrdlsrkswnl.tistory.com/"
-              target="_blank"
-            >
-              <TistoryIcon className="w-4 h-4" fill="currentColor" />
-            </a>
-
-            <a
+              text={<TistoryIcon className="w-4 h-4" fill="currentColor" />}
               className="flex items-center justify-center ml-3 text-gray-500 cursor-pointer"
+            />
+            <CommonLink
               href="https://www.instagram.com/hojun456"
-              target="_blank"
-            >
-              <InstagramIcon className="w-5 h-5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-            </a>
+              text={<InstagramIcon className="w-5 h-5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />}
+              className="flex items-center justify-center ml-3 text-gray-500 cursor-pointer"
+            />
           </span>
         </div>
       </div>

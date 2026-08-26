8e5b99ec70007a324ff0b89475bf72d9aedaf6b0
hojun.yoon <tjdsksro90@gmail.com>
2026-08-26 17:02:13 +0900

    fix: lottie 미리 생성 방지

--- components/home/Animation.tsx
+++ components/home/Animation.tsx
@@ -1,5 +1,10 @@
-import Lottie from 'react-lottie-player';
+import dynamic from 'next/dynamic';
 import lottieJson from '../../public/animaion.json';
+
+// react-lottie-player를 서버(SSR)가 아닌 브라우저(클라이언트)에서만 로드
+const Lottie = dynamic(() => import('react-lottie-player'), {
+  ssr: false,
+});
 
 const Animation = () => {
   return <Lottie loop={false} animationData={lottieJson} play />;
